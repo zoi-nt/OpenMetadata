f9e84fe7d72a59aa94e52aab773edf61ab7ea074
nithin.teekaramanaa <nithin.teekaramanaa@zoi.tech>
2025-11-11 20:44:49 +0100

    adding generated types

---
+++ ingestion/src/metadata/generated/antlr/EntityLinkListener.py
@@ -0,0 +1,57 @@
+# Generated from /Users/nithinteekaramanaa/Documents/zoi/projects/kaercher/OpenMetadata/openmetadata-spec/src/main/antlr4/org/openmetadata/schema/EntityLink.g4 by ANTLR 4.9.3
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .EntityLinkParser import EntityLinkParser
+else:
+    from EntityLinkParser import EntityLinkParser
+
+# This class defines a complete listener for a parse tree produced by EntityLinkParser.
+class EntityLinkListener(ParseTreeListener):
+
+    # Enter a parse tree produced by EntityLinkParser#entitylink.
+    def enterEntitylink(self, ctx:EntityLinkParser.EntitylinkContext):
+        pass
+
+    # Exit a parse tree produced by EntityLinkParser#entitylink.
+    def exitEntitylink(self, ctx:EntityLinkParser.EntitylinkContext):
+        pass
+
+
+    # Enter a parse tree produced by EntityLinkParser#entityType.
+    def enterEntityType(self, ctx:EntityLinkParser.EntityTypeContext):
+        pass
+
+    # Exit a parse tree produced by EntityLinkParser#entityType.
+    def exitEntityType(self, ctx:EntityLinkParser.EntityTypeContext):
+        pass
+
+
+    # Enter a parse tree produced by EntityLinkParser#nameOrFQN.
+    def enterNameOrFQN(self, ctx:EntityLinkParser.NameOrFQNContext):
+        pass
+
+    # Exit a parse tree produced by EntityLinkParser#nameOrFQN.
+    def exitNameOrFQN(self, ctx:EntityLinkParser.NameOrFQNContext):
+        pass
+
+
+    # Enter a parse tree produced by EntityLinkParser#entityField.
+    def enterEntityField(self, ctx:EntityLinkParser.EntityFieldContext):
+        pass
+
+    # Exit a parse tree produced by EntityLinkParser#entityField.
+    def exitEntityField(self, ctx:EntityLinkParser.EntityFieldContext):
+        pass
+
+
+    # Enter a parse tree produced by EntityLinkParser#separator.
+    def enterSeparator(self, ctx:EntityLinkParser.SeparatorContext):
+        pass
+
+    # Exit a parse tree produced by EntityLinkParser#separator.
+    def exitSeparator(self, ctx:EntityLinkParser.SeparatorContext):
+        pass
+
+
+
+del EntityLinkParser
